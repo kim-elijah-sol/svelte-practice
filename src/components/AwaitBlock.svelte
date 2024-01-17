c1ae14a540668b88caf29909a4f01c2fd87ea138
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-17 16:20:12 +0900

    feat: await block > catch 상황 추가

--- src/components/AwaitBlock.svelte
+++ src/components/AwaitBlock.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
     let list = getList()
 
+    function randomError() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (Math.random() > 0.5) {
+                    resolve('success')
+                } else {
+                    reject('Random Error')
+                }
+            }, 1000)
+        })
+    }
+
     async function getList (){
+        await randomError()
         const res = await fetch('https://jsonplaceholder.typicode.com/todos')
         const data = await res.json()
 
         return data
     }
+
+    function handleClick() {
+        list = getList()
+    }
 </script>
 
+<div>
+    <button on:click={handleClick}>
+        Re-try
+    </button>
+</div>
 
 {#await list}
     <p>Data Loading...</p>
@@ -18,4 +40,6 @@
             <li>{item.title}</li>
         {/each}
     </ul>
+{:catch error}
+    <p style="color: red;">{error}</p>
 {/await}
